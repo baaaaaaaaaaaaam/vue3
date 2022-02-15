153837b144637af09274b6a79fd26bec75d8d315
ansgyqja <ansgyqja@naver.com>
2022-02-15 20:48:35 +0900

    따라해보기3-5

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,16 @@
   <!-- <Watch msg="Welcome to Your Vue.js App"/> -->
   <!-- <Props msg="Welcome to Your Vue.js App" :data="[1,2,3]"/> -->
   <!-- <EmitParent msg="Welcome to Your Vue.js App"/> -->
-  <Ex1/>
+  <!-- <Ex1/> -->
+  <!-- <MyButton sm> MyButton Test </MyButton>
+  <MyButton lg> MyButton Test </MyButton>
+  <MyButton pill> MyButton Test </MyButton>
+  <MyButton background-white pill> MyButton Test </MyButton> -->
+  <MyButton type="switch" v-model:active="active"> MyButton Test </MyButton>
+  <!-- <MyButton type="switch" sep="2" v-model:active="active" @click="onClicked"> MyButton Test </MyButton> -->
 </template>
 
-<script setup>
+<script>
 // import LifeCycle from './components/LifeCycle.vue'
 // import Vmodel from './components/Vmodel.vue'
 // import Reactive from './components/Reactive.vue'
@@ -18,7 +24,25 @@
 // import Watch from './components/Watch.vue'
 // import Props from './components/Props.vue'
 // import EmitParent from './components/EmitParent.vue'
-import Ex1 from './components/Practice/Ex3-5-1.vue'
+// import Ex1 from './components/Practice/Ex3-5-1.vue'
+import {ref, watch} from 'vue'
+import MyButton from './components/Practice/Ex3-5-2.vue'
+
+export default {
+  components:{
+    MyButton
+  },
+  setup(){
+    const active=ref(true)
+    watch(active,()=>{
+      console.log(active.value)
+    })
+    return {
+      active,
+    }
+  }
+}
+
 </script>
 
 <style>
